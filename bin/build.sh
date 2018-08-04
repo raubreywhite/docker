4397ab00cbef9939ec37ebe41e407b72cf8d9485
Richard White <r@rwhite.no>
2018-08-04 19:59:36 +0300

    tensorflow

--- bin/build.sh
+++ bin/build.sh
@@ -9,3 +9,7 @@ source $CI_PWD/docker/bin/R_VERSION.sh
 
 docker build --pull --no-cache --tag=raw996/dhadley:$RVERSION --build-arg RVERSION=$RVERSION $CI_PWD/docker/dhadley
 
+docker build --pull --no-cache --tag=raw996/dp:gpu --build-arg FROM_IMAGE=nvcr.io/nvidia/tensorflow:18.04-py3 $CI_PWD/docker/dp
+
+docker build --pull --no-cache --tag=raw996/dp:cpu --build-arg FROM_IMAGE =tensorflow/tensorflow:1.10.0-rc1-py3 $CI_PWD/docker/dp
+
